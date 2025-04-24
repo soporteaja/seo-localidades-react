9fec99a0e41ca4bd39965015582dcd490b38ba09
unknown <soporte@ajapublicidad.es>
2025-04-24 13:44:05 +0200

    Migración a Vite completada y mejoras en la UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import CSVProcessor from "./components/CSVProcessor";
-import ErrorBoundary from "./ErrorBoundary";
 
 function App() {
   return (
-    <ErrorBoundary>
-      <div>
-        <CSVProcessor />
-      </div>
-    </ErrorBoundary>
+    <div>
+      <CSVProcessor />
+    </div>
   );
 }
 
